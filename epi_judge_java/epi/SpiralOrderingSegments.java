61d4a4bc0e8264a22a40ce49205857dd4ee0ebd6
Ryan Zhou <ryanzhou7@gmail.com>
2019-02-08 10:02:32 -0800

    Finished 7.1 Interconvert string and int

--- epi_judge_java/epi/SpiralOrderingSegments.java
+++ epi_judge_java/epi/SpiralOrderingSegments.java
@@ -8,7 +8,55 @@ public class SpiralOrderingSegments {
   @EpiTest(testDataFile = "spiral_ordering_segments.tsv")
 
   public static List<Integer> matrixInSpiralOrder(List<List<Integer>> squareMatrix) {
-    return null;
+    List<Integer> list = new ArrayList<>();
+    if(squareMatrix.size()==0)
+      return list;
+    if(squareMatrix.size()==1) {
+      list.add(squareMatrix.get(0).get(0));
+      return list;
+    }
+    int cRow = 0;
+    int cCol = 0;
+    int rightCol = squareMatrix.get(0).size();
+    int rightRow = squareMatrix.get(0).size();
+    int leftCol = 0;
+    int leftRow = 1;
+    while (true){
+      //RIGHT
+      while(cCol!=rightCol){
+        list.add(squareMatrix.get(cRow).get(cCol++));
+      }
+      cCol--;
+      rightCol--;
+      if(cRow+1>=rightRow)
+        break;
+      cRow++;
+      //DOWN
+      while(cRow!=rightRow){
+        list.add(squareMatrix.get(cRow++).get(cCol));
+      }
+      cRow--;
+      rightRow--;
+      cCol--;
+      //LEFT
+      while (cCol!=leftCol){
+        list.add(squareMatrix.get(cRow).get(cCol--));
+      }
+      cCol++;
+      leftCol++;
+      if(cRow-1<=leftRow)
+        break;
+      cRow--;
+      //UP
+      while(cRow!=leftRow){
+        list.add(squareMatrix.get(cRow--).get(cCol));
+      }
+      cCol++;
+      cRow++;
+      leftRow--;
+    }
+    list.add(squareMatrix.get(cRow).get(--cCol));
+    return list;
   }
 
   public static void main(String[] args) {
--- epi_judge_java/epi/StringIntegerInterconversion.java
+++ epi_judge_java/epi/StringIntegerInterconversion.java
@@ -5,12 +5,38 @@ import epi.test_framework.TestFailure;
 public class StringIntegerInterconversion {
 
   public static String intToString(int x) {
-    // TODO - you fill in here.
-    return "";
+    StringBuilder sb = new StringBuilder();
+    boolean isNeg = false;
+    if(x<0){
+      isNeg = true;
+    }
+    do{
+      sb.append((char)(Math.abs(x%10) +'0'));
+      x /= 10;
+    }while(x!=0);
+    if(isNeg){
+      sb.append("-");
+    }
+    sb.reverse();
+    return sb.toString();
   }
   public static int stringToInt(String s) {
-    // TODO - you fill in here.
-    return 0;
+    boolean isNegative = false;
+    if(s.charAt(0)=='-'){
+      isNegative = true;
+      s = s.substring(1);
+    }
+    int num = 0;
+    int base = 1;
+    for(int i = s.length()-1; i>=0; i--){
+      char c = s.charAt(i);
+      int temp = (int)(c-'0');
+      num += (temp*base);
+      base *= 10;
+    }
+    if(isNegative)
+      num *= -1;
+    return num;
   }
   @EpiTest(testDataFile = "string_integer_interconversion.tsv")
   public static void wrapper(int x, String s) throws TestFailure {
@@ -20,6 +46,25 @@ public class StringIntegerInterconversion {
     if (stringToInt(s) != x) {
       throw new TestFailure("String to int conversion failed");
     }
+  }
+
+  public static int indexOfDifference(String str1, String str2) {
+    if (str1 == str2) {
+      return -1;
+    }
+    if (str1 == null || str2 == null) {
+      return 0;
+    }
+    int i;
+    for (i = 0; i < str1.length() && i < str2.length(); ++i) {
+      if (str1.charAt(i) != str2.charAt(i)) {
+        break;
+      }
+    }
+    if (i < str2.length() || i < str1.length()) {
+      return i;
+    }
+    return -1;
   }
 
   public static void main(String[] args) {
