3bf8eb3b2c1b2d14da019912a822b2d0078a47e9
Ryan Zhou <ryanzhou7@gmail.com>
2019-02-14 16:19:22 -0800

    Finished 8.1 Merge two sorted lists

--- epi_judge_java/epi/SortedListsMerge.java
+++ epi_judge_java/epi/SortedListsMerge.java
@@ -6,8 +6,40 @@ public class SortedListsMerge {
   //@include
   public static ListNode<Integer> mergeTwoSortedLists(ListNode<Integer> L1,
                                                       ListNode<Integer> L2) {
-    // TODO - you fill in here.
-    return null;
+    if(L1 == null)
+      return L2;
+    if(L2 == null)
+      return L1;
+    ListNode<Integer> head;
+    ListNode<Integer> c;
+    if(L1.data < L2.data){
+      c = L1;
+      head = L1;
+      L1 = L1.next;
+    }
+    else{
+      c = L2;
+      head = L2;
+      L2 = L2.next;
+    }
+    while(L1 != null && L2 != null){
+      if(L1.data <= L2.data){
+        c.next = L1;
+        L1 = L1.next;
+      }
+      else{
+        c.next = L2;
+        L2 = L2.next;
+      }
+      c = c.next;
+    }
+    if(L1==null){
+      c.next = L2;
+    }
+    else{
+      c.next = L1;
+    }
+    return head;
   }
 
   public static void main(String[] args) {
