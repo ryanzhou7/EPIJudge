d8c94ffa9ef70e5eebdbf0e9110a1d05181c2fd3
Ryan Zhou <ryanzhou7@gmail.com>
2019-02-19 09:58:09 -0800

    Debug 8.2 Reverse Sublist(linked-list)

--- epi_judge_java/epi/ReverseSublist.java
+++ epi_judge_java/epi/ReverseSublist.java
@@ -1,13 +1,43 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.Stack;
+
 public class ReverseSublist {
   @EpiTest(testDataFile = "reverse_sublist.tsv")
 
   public static ListNode<Integer> reverseSublist(ListNode<Integer> L, int start,
                                                  int finish) {
-    // TODO - you fill in here.
-    return null;
+    if(start==finish)
+      return L;
+    ListNode<Integer> dummy = new ListNode<>(0, L);
+    ListNode<Integer> curr = L;
+    int c = 1;
+    while(c++!= start){
+      curr = curr.next;
+    }
+    ListNode<Integer> bs = curr;
+    ListNode<Integer> af = null;
+    ListNode<Integer> ns = null;
+    c++;
+    curr = curr.next;
+    ListNode<Integer> nf = curr;
+    ListNode<Integer> next = curr.next;
+    while(c != finish-2){
+      ListNode<Integer> nn = next.next;
+      next.next = curr;
+      curr = next;
+      next = nn;
+      c++;
+      if(c==finish-2){
+        af = nn;
+        ns = next;
+      }
+    }
+    bs.next = ns;
+    nf.next = af;
+    return dummy.next;
   }
 
   public static void main(String[] args) {
