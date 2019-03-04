42db67992559a0dd158f17807edb07200f415697
Ryan Zhou <ryanzhou7@gmail.com>
2019-03-04 09:31:19 -0800

    Finished 9.7 Tree Level Order

--- epi_judge_java/epi/TreeLevelOrder.java
+++ epi_judge_java/epi/TreeLevelOrder.java
@@ -1,14 +1,35 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 public class TreeLevelOrder {
   @EpiTest(testDataFile = "tree_level_order.tsv")
 
-  public static List<List<Integer>>
-  binaryTreeDepthOrder(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return null;
+  public static List<List<Integer>> binaryTreeDepthOrder(BinaryTreeNode<Integer> tree) {
+    List<List<Integer>> listList = new ArrayList<>();
+    if(tree==null)
+      return listList;
+    Deque<BinaryTreeNode<Integer>> cLevel = new LinkedList<>();
+    cLevel.addLast(tree);
+    while(!cLevel.isEmpty()){
+      Deque<BinaryTreeNode<Integer>> nLevel = new LinkedList<>();
+      List<Integer> list = new ArrayList<>();
+      while(!cLevel.isEmpty()){
+        BinaryTreeNode<Integer> c = cLevel.removeFirst();
+        list.add(c.data);
+        if(c.left!=null)
+          nLevel.addLast(c.left);
+        if(c.right!=null)
+          nLevel.addLast(c.right);
+      }
+      listList.add(list);
+      cLevel = nLevel;
+    }
+    return listList;
   }
 
   public static void main(String[] args) {
