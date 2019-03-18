3bc5b49c87b46492c08e60885c9e99621c804227
Ryan Zhou <ryanzhou7@gmail.com>
2019-03-18 09:52:44 -0700

    Finished 13.2 Is anonymous letter constructible

--- epi_judge_java/epi/IsAnonymousLetterConstructible.java
+++ epi_judge_java/epi/IsAnonymousLetterConstructible.java
@@ -1,13 +1,38 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class IsAnonymousLetterConstructible {
   @EpiTest(testDataFile = "is_anonymous_letter_constructible.tsv")
 
   public static boolean isLetterConstructibleFromMagazine(String letterText,
                                                           String magazineText) {
-    // TODO - you fill in here.
-    return true;
+    Map<Character, Integer> letterFreq = new HashMap<>();
+    for(char c: letterText.toCharArray()){
+      if(letterFreq.containsKey(c)){
+        letterFreq.put(c, letterFreq.get(c)+1);
+      }
+      else{
+        letterFreq.put(c, 1);
+      }
+    }
+    for(char c: magazineText.toCharArray()){
+      if(letterFreq.containsKey(c)){
+        Integer freq = letterFreq.get(c);
+        if(freq==1){
+          letterFreq.remove(c);
+          if(letterFreq.isEmpty())
+            return true;
+        }
+        else{
+          letterFreq.put(c, freq-1);
+        }
+      }
+    }
+    return letterFreq.isEmpty();
   }
 
   public static void main(String[] args) {
