c2dd309b64a3106c4c0a8fc28ecf6e9cea7c6030
Ryan Zhou <ryanzhou7@gmail.com>
2019-03-18 14:09:44 -0700

    Finished 12.1 search array for first occurence of k

--- epi_judge_java/epi/SearchFirstKey.java
+++ epi_judge_java/epi/SearchFirstKey.java
@@ -6,8 +6,27 @@ public class SearchFirstKey {
   @EpiTest(testDataFile = "search_first_key.tsv")
 
   public static int searchFirstOfK(List<Integer> A, int k) {
-    // TODO - you fill in here.
-    return 0;
+    int L = 0;
+    int H = A.size()-1;
+    int iLast = -1;
+    while(L<=H){
+      int M = calc(L, H);
+      if(A.get(M)==k){
+        //want to be looking for first occurrence
+        if(M==0 || A.get(M-1)!=k)
+          return M;
+        H = M-1;
+        iLast = M;
+      }
+      else if(A.get(M)> k)
+        H = M-1;
+      else
+        L = M+1;
+    }
+    return iLast;
+  }
+  public static int calc(int L, int H){
+    return (H-L)/2 + L;
   }
 
   public static void main(String[] args) {
